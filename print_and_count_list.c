120224ce63cde0e556765e9bcadc2a91496ac49d
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-11 03:55:02 +0100

    changes: moved and added more functions to this file to aid better functionality

--- print_and_count_list.c
+++ print_and_count_list.c
@@ -7,7 +7,7 @@
 */
 int count_elements (struct node *head) {
 	struct node *temp = head; // temp is used to traverse the list
-	int count = 0, i;
+	int count = 0;
 
 	/* counts the number of elements i.e nodes */
 	while (temp != NULL) {
@@ -32,24 +32,27 @@ void print_list (struct node * head, int count) {
 
 	/* prints the elements in the form of an array */
 	putchar('[');
-	// temp = head; // resets the temp pointer to the head node again
 	int i = 0;
 	while (i < count) {
 		printf ("%d", temp->data); // prints the data part of the current node
-		length += lenth_of_numbers(temp->data); 
+		length += lenth_of_numbers(temp->data); // accumulates the lengths of each data parts of the nodes
 		temp = temp->next; // moves the pointer to the next node
 		(i != (count - 1)) && printf(", ");
 		i++;
 	}
 	printf("]");
 	putchar('\n');
-	for(i = 0; i < (length+(count*3)+10); i++) {
-		printf(".");
+	for(i = 0; i < (length+(count*2)+8); i++) {
+		printf("."); // dynamically prints the doted underlines
 	}
 	putchar('\n');
 }
 
-
+/*
+ * lenth_of_numbers functiom
+ * @number - the number to be counted
+ * returns the length of number
+ */
 int lenth_of_numbers(int number) {
 	int length;
 	char str[10]; // Assuming the number won't exceed 10 digits
@@ -57,4 +60,46 @@ int lenth_of_numbers(int number) {
 	length = strlen(str); // Find the length of the string 
 	// printf("Number of digits: %d\n", length); 
 	return length;
+}
+
+/*
+ * converts_input_to_integer - function
+ * @newNode - pointer to the node to whose address part needs to be filled
+ * @skip - number to decide whether to skip printing the error message
+ * return - 0 on success
+ */
+int converts_input_to_integer(struct node *newNode, int skip) {
+	char input[5];
+	int number;
+
+	scanf("%s", input);
+	number = atoi(input); // tries to convert the input to an integer
+
+	/* checks if the input is an integer or if the user is done entering data */
+	if (number != 0 || input[0] == '0') {
+		if (newNode != NULL) {
+			newNode->data = number; // stores data into the node
+		} else {
+			number = number; // updates the number variable
+			return number;
+		}
+	} else {
+		(skip) && printf("Invalid entry\n"); 
+		return 50; // exits the operation
+	}
+	return 0;
+}
+
+/*
+ * confirm_space_allocation - confirm space allocation
+ * @newNode - pointer to be checked
+ * returns 0 if successful
+ */
+int confirm_space_allocation (struct node *newNode) {
+	/* checks that the space is allocated for the node */
+	if (newNode == NULL) {
+		printf ("Could not allocate space");
+		return 1;
+	}
+	return 0;
 }
