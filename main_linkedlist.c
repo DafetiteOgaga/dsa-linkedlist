cd701f14a2bf70c2f540dd31c13d71fda71608d5
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-11 03:54:15 +0100

    changes: modified the main function to accomodate the capability of inserting nodes at the beginning, end and any position on the linked list

--- main_linkedlist.c
+++ main_linkedlist.c
@@ -5,28 +5,25 @@
  * returns - 0 on success
  * */
 int main (void) {
-	struct node * head = create_nodes();
+	struct node *head;
 	int count, number = 1000, ret;
-	char input [5];
 
+	head = create_nodes();
+	if (head == NULL) {
+		return (1);
+	}
 	// printf("The head of the nodes is at: %p\n", head);
 	count = count_elements (head); // invoke the count_elements function to print the number of elements
 	print_list (head, count); // invoke the print_list function to print the list of elements like an array
-	// add_node_to_beginning
 
 	while (number) {
 		putchar('\n');
 		printf ("Press 1: To add a new head node\n\
-Press 2: To add nede to any position\n\
+Press 2: To add node to any position\n\
 Press 3: To add node to the foot node\n");
 		printf (" >>> ");
-		scanf("%s", input);
-		number = atoi(input); // tries to convert the input to an integer
-		/* checks if the input is an integer or if the user is done entering data */
-		if (number != 0 || input[0] == '0') {
-			number = number; // stores data into the node
-		} else {
-			// putchar('\n');
+		number = converts_input_to_integer(NULL, 1); // converts input to integer
+		if (number == 50) {
 			count = count_elements (head);
 			print_list (head, count);
 			break; // exit if the input is not an integer
@@ -34,13 +31,16 @@ Press 3: To add node to the foot node\n");
 
 		if (number == 1) {
 			head = add_node_to_beginning(head);
+			if (head == NULL) {
+				break;
+			}
 		} else if (number == 2) {
 			ret = add_node_to_any_position(head, count);
-			if (ret) {
-				continue;
-			}
 		} else if (number == 3) {
-			add_node_to_end(head);
+			ret = add_node_to_end(head);
+		} else {
+			printf("Invalid entry\n");
+            // continue;
 		}
 		count = count_elements (head);
 		print_list (head, count);
