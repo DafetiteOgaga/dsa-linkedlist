fe0e681e46b54fbacfd6ecc3b74ad829720bfdb6
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-11 03:53:00 +0100

    changes: added string header file, more function prototypes for the linkedlist algorithm

--- linkedlist.h
+++ linkedlist.h
@@ -3,23 +3,27 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-/*                                                                 * Structure of the node used in the linked list
+/*
+ * Structure of the node used in the linked list
  * @data (int) - the data part of the node
  * @next (pointer) - the address part of the node
  */
 struct node {
 	int data;
 	struct node * next;
- };
+};
 
 /* prototypes */
 struct node * create_nodes (void);
 int count_elements (struct node *);
 void print_list (struct node *, int);
 struct node * add_node_to_beginning(struct node *);
-void add_node_to_end(struct node *);
+int add_node_to_end(struct node *);
 int add_node_to_any_position(struct node *, int);
 int lenth_of_numbers(int);
+int converts_input_to_integer(struct node *, int);
+int confirm_space_allocation (struct node *);
 
 #endif
