edb29eb8dca886c0338cdb27bc9bfa6fbdeef691
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2024-05-11 03:48:12 +0100

    new: contains function to insert a node into any position aside beginning and the end of the list

--- add_to_any_position_of_list.c
+++ add_to_any_position_of_list.c
@@ -2,68 +2,38 @@
 
 /*
  * add_node_to_any_position function - inserts a node into any position in the list
+ * return 0 if successful
  */
 int add_node_to_any_position (struct node *head, int count) {
 	/* declarations and definitions */
-	struct node *newNode, *temp = head;
-	char input [5];
-	int number, position, i = 1;
-
+	struct node *newNode, *temp = head; // points temp to head
+	int position, i = 1, resp, check;
 
 	printf("Choose a position between [1] and [%d] >>> ", count);
-	scanf("%d", &position);
-	if (position > count) {
-		putchar('\n');
-		printf("Invalid position\n");
-		return 1;
+	position = converts_input_to_integer(NULL, 1);
+	if ((position < 0 || position > count) || (position == 50)) {
+		return position;
 	}
 
-
-	
-	// newNode = (struct node *) malloc (sizeof(struct node)); // node creation in memory
-
-	// temp = head;
 	while (i < (position-1)) {
 		temp = temp->next;
-		// printf("count: %d ::: addy: %p\n", i, temp);
 		i++;
 	}
 
-
 	newNode = (struct node *) malloc (sizeof(struct node)); // node creation in memory
-								//
-	/* checks that the space is allocated for the node */
-	if (newNode == NULL) {
-		printf ("Could not allocate space");
-		exit(1);
+	check = confirm_space_allocation (newNode); // checks if the allocation was successful
+	if (check) {
+		return check;
 	}
 
-	// printf("Choose a position between [1] and [%d] >>> ", count);
-	// scanf("%d", &position);
-	// if (position >= count) {
-	//	printf("Invalid position\n");
-	//	exit(1);
-	// }
-	putchar('\n');
-	printf ("Enter the data >>> ");
-	scanf("%s", input);
-	number = atoi(input); // tries to convert the input to an integer
-
-	/* checks if the input is an integer or if the user is done entering data */
-	if (number != 0 || input[0] == '0') {
-		newNode->data = number; // stores data into the node
-	} else {
-		printf("Invalid entry\n"); // exits the operation
+	printf ("Enter the data for position %d >>> ", position);
+	resp = converts_input_to_integer(newNode, 1); // converts input to integer
+	if (resp) {
+		return resp;
 	}
 
-	// temp = head;
-	// while (i < (position-1)) {
-	// 	temp = temp->next;
-	//	printf("count: %d ::: addy: %p\n", i, temp);
-	//	i++;
-	// }
-	newNode->next = temp->next; // sets the node's pointer to null
-	temp->next  = newNode; // sets the last node to point to the new node
+	newNode->next = temp->next; // sets the new node's pointer to the next node
+	temp->next  = newNode; // sets the previous node to point to the new node
 	return 0;
 }
 
